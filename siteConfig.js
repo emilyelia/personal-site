229d0f3df66dc0dd9c9d95639b10e987c6e2720a
emilyelia <emily1997@gmail.com>
2020-12-05 18:41:42 -0500

    inital commit for personal webstie

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Emily Elia',
+  shortName: 'Emily Elia',
+  description: 'Add quick bio',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Emily Elia',
+  twitter: 'Emily_Elia'
+}
